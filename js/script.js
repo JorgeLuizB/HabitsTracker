14b156ef0960e49782800c0c85a4b30ea5f8eb02
JorgeLuiz <jrgluiz4848@gmail.com>
2023-01-20 21:34:53 -0300

    implementado funcao para add dias

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const form = document.querySelector('#form-habits');
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
+button.addEventListener('click', add);
 
-const data = {
+function add(){
+    const today = new Date().toLocaleDateString('pt-br').slice(0,-5);
+    const dayExists = nlwSetup.dayExists(today);
+
+    if(dayExists){
+        alert('Este dia já existe');
+        return;
+    }
+
+    nlwSetup.addDay(today);
+}
+
+/*const data = {
     run: ['01-01','01-02','01-06'],
     water: ['01-04','01-05'],
     food: ['01-01','01-03'],
-}
+}*/
 nlwSetup.setData(data);
 nlwSetup.load();
