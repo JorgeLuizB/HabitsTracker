36bd6902d6dddd5f493ec94f55b4e370396c2750
JorgeLuiz <jrgluiz4848@gmail.com>
2023-01-20 22:05:15 -0300

    verifica se existe algum dado salvo

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const nlwSetup = new NLWSetup(form);
 const button = document.querySelector('header button');
 button.addEventListener('click', add);
 form.addEventListener('change', save);
-
 function add(){
     const today = new Date().toLocaleDateString('pt-br').slice(0,-5);
     const dayExists = nlwSetup.dayExists(today);
@@ -18,7 +17,6 @@ function add(){
 function save(){
     localStorage.setItem('NWLSetup@habits', JSON.stringify(nlwSetup.data));
 }
-
-const data = JSON.parse(localStorage.getItem('NWLSetup@habits'));
+const data = JSON.parse(localStorage.getItem('NWLSetup@habits')) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
